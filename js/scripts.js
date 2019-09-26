e2104a0d325f6bdcf53cd628fe61c8b01ad0f8ff
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-26 10:44:17 +0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('h1').click(function(){
+        alert("This is a heading tag")
+        alert("I TOLD YOU THIS IS A HEADER!")
+    });
+    
+    $('p').click(function(){
+        alert("This is a paragraph")
+    });
+    
+    $('img').click(function(){
+        alert("This is an image")
+    });
+});
